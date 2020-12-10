2fbf73cb47b44033c25c1219d207746ba35bd4e3
Eduardo Goncalves <dudssource@gmail.com>
2020-12-10 17:23:24 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+setuptools.setup(
+    name='vvpctl',
+    version='0.1.0',    
+    description='Ververica Platform Cli',
+    url='https://github.com/3bit-techs/vvpctl',
+    author='Eduardo Goncalves',
+    author_email='dudssource@gmail.com',
+    packages=setuptools.find_packages(),
+    scripts=['vvpctl'],
+    install_requires=[
+        'requests==2.25.0',
+        'DeepDiff==5.0.2',
+    ],
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
